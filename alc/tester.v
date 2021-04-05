5a3714356ca08de7c91aa7a365dc84d9ac3501ff
Duarte Roso <duarte.roso@posteo.eu>
2021-04-05 15:37:04 +0200

    Wrap test function in test conditional

--- alc/tester.v
+++ alc/tester.v
@@ -2,18 +2,20 @@ module alc
 
 // do_test is used to test functionality
 pub fn do_test(cb fn ()) {
-	mut device := new_device()
-	device.open_default()
-	defer {
-		device.close()
+	$if test {
+		mut device := new_device()
+		device.open_default()
+		defer {
+			device.close()
+		}
+		//
+		mut context := new_context()
+		context.create(device)
+		defer {
+			context.destroy()
+		}
+		context.make_current()
+		//
+		cb()
 	}
-	//
-	mut context := new_context()
-	context.create(device)
-	defer {
-		context.destroy()
-	}
-	context.make_current()
-	//
-	cb()
 }
