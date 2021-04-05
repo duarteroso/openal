ab2418f6da09981af9ef044d3074ae3e41d627b6
Duarte Roso <duarte.roso@posteo.eu>
2021-04-05 18:39:41 +0200

    Workaround conditional/defer bug

--- alc/tester.v
+++ alc/tester.v
@@ -3,19 +3,28 @@ module alc
 // do_test is used to test functionality
 pub fn do_test(cb fn ()) {
 	$if test {
-		mut device := new_device()
-		device.open_default()
-		defer {
-			device.close()
-		}
-		//
-		mut context := new_context()
-		context.create(device)
-		defer {
-			context.destroy()
-		}
-		context.make_current()
-		//
-		cb()
+		concrete_test(cb)
+	} $else {
+		dummy_test()
 	}
+}
+
+fn dummy_test() {
+}
+
+fn concrete_test(cb fn ()) {
+	mut device := new_device()
+	device.open_default()
+	defer {
+		device.close()
+	}
+	//
+	mut context := new_context()
+	context.create(device)
+	defer {
+		context.destroy()
+	}
+	context.make_current()
+	//
+	cb()
 }
