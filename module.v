5cffa03bb2174217476987401e6e9fdfdac8a6f2
Duarte Roso <duarteroso@users.noreply.github.com>
2022-10-26 22:53:58 +0200

    Block non-supported platforms from building

--- module.v
+++ module.v
@@ -2,6 +2,10 @@ module openal
 
 import v.vmod
 
+$if !linux {
+	$compile_error('Only Linux is currently supported') 
+}
+
 // manifest of module
 pub fn manifest() ?vmod.Manifest {
 	return vmod.decode(@VMOD_FILE)
